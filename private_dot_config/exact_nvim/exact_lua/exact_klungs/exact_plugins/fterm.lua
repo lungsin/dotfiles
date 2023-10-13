a929124d6d9f279fe0fd0fdef4feb12e4f4e65a6
Kwee Lung Sin <kweelungsin@gmail.com>
2023-10-14 06:42:19 +0800

    Add fterm and tree-sitter-context

---
+++ private_dot_config/exact_nvim/exact_lua/exact_klungs/exact_plugins/fterm.lua
@@ -0,0 +1,10 @@
+return {
+    "numToStr/FTerm.nvim",
+    config = function()
+        local fterm = require('FTerm')
+        -- fterm.setup({})
+
+        vim.keymap.set('n', '<C-_>', '<CMD>lua require("FTerm").toggle()<CR>')
+        vim.keymap.set('t', '<C-_>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+    end
+}
