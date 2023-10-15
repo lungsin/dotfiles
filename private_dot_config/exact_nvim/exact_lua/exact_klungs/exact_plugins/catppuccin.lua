4f80b527e846707e7e55b51437a673a8665e024d
Kwee Lung Sin <kweelungsin@gmail.com>
2023-10-15 21:37:08 +0900

    Add gitsigns and move config of catpuccin to lazy spec

--- private_dot_config/exact_nvim/exact_lua/exact_klungs/exact_plugins/catppuccin.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_klungs/exact_plugins/catppuccin.lua
@@ -1,1 +1,13 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "macchiato", -- latte, frappe, macchiato, mocha
+        })
+
+        -- setup must be called before loading
+        vim.cmd.colorscheme "catppuccin"
+    end,
+}
--- private_dot_config/exact_nvim/exact_lua/exact_klungs/exact_plugins/git.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_klungs/exact_plugins/git.lua
@@ -2,4 +2,6 @@ return {
     -- Git related plugins
     'tpope/vim-fugitive',
     'tpope/vim-rhubarb',
+    -- gitsigns
+    'lewis6991/gitsigns.nvim', -- TODO: configure gitsigns
 }
