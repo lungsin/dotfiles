bc75cebbfc2656c9f186169bf6e0bad38e9a089c
Kwee Lung Sin <kweelungsin@gmail.com>
2024-03-25 00:19:58 +0800

    Disable fterm

--- private_dot_config/exact_nvim/exact_lua/exact_klungs/exact_plugins/fterm.lua
+++ private_dot_config/exact_nvim/exact_lua/exact_klungs/exact_plugins/fterm.lua
@@ -1,10 +1,11 @@
 return {
-    'numToStr/FTerm.nvim',
-    config = function()
-        -- local fterm = require('FTerm')
-        -- fterm.setup({})
+	"numToStr/FTerm.nvim",
+	enabled = true,
+	config = function()
+		-- local fterm = require('FTerm')
+		-- fterm.setup({})
 
-        vim.keymap.set('n', '<C-_>', '<CMD>lua require("FTerm").toggle()<CR>')
-        vim.keymap.set('t', '<C-_>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
-    end
+		vim.keymap.set("n", "<C-_>", '<CMD>lua require("FTerm").toggle()<CR>')
+		vim.keymap.set("t", "<C-_>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+	end,
 }
